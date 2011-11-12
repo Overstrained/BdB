01fd40f5bdbbf8bb649905d08b005af349affa40
Stephan Schultheiß <stephan.schultheiss@gmail.com>
2011-11-12 19:51:00 +0100

    Sql Skript hinzugefügt.

---
+++ sql/initial.sql
@@ -0,0 +1,8 @@
+create database BdB;
+
+CREATE TABLE persons (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  firstname varchar(255) DEFAULT NULL,
+  lastname varchar(255) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
